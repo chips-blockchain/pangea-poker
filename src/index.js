043d8c456da073d043a2ec4eedabf5bfca0827c2
Norbert Dragán <norbert.dragan@gmail.com>
2019-06-05 15:14:23 +0800

    Add Noticia font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Global, css } from "@emotion/core";
 import Table from "./components/Table";
 import "normalize.css";
+import NoticiaBold from "./fonts/NoticiaText-Bold.ttf";
+
 class App extends React.Component {
   render() {
-    return <Table />;
+    return (
+      <React.Fragment>
+        <Global
+          styles={css`
+            @font-face {
+              font-family: "Noticia Text";
+              font-style: normal;
+              font-weight: 700;
+              font-display: swap;
+              src: local("Noticia Text Bold"), local("NoticiaText-Bold"),
+                url(${NoticiaBold}) format("woff2");
+            }
+            * {
+              font-family: "Noticia Text";
+            }
+          `}
+        />
+        <Table />
+      </React.Fragment>
+    );
   }
 }
 
