54e0765faf5c3971acc5f5348a5e2feee91a222e
Norbert Dragán <norbert.dragan@gmail.com>
2019-06-10 11:58:52 +0800

    Fix chip text alignment on the right side

--- src/components/Chips/Bet.js
+++ src/components/Chips/Bet.js
@@ -9,19 +9,34 @@ const Bet = props => {
   return (
     <span
       css={css`
+        align-items: center;
         grid-area: ${props.forPlayer};
         display: grid;
-        grid-template-columns: 1.25rem 4rem;
+        position: relative;
+        ${props.forPlayer === "player-2" || props.forPlayer === "player-3"
+          ? "grid-template-columns: 4rem 1.25rem; left: -3.5rem;"
+          : "grid-template-columns: 1.25rem 4rem;"}
       `}
     >
-      <Chip />
       <span
         css={css`
+          ${props.forPlayer === "player-2" || props.forPlayer === "player-3"
+            ? "order: 2;"
+            : "order: 1;"}
+        `}
+      >
+        <Chip />
+      </span>
+      <span
+        css={css`
+          order: 1;
           color: ${theme.moon.colors.text};
           font-size: 0.625rem;
+          padding: 0 0 0.25rem 0.5rem;
+          position: relative;
           ${props.forPlayer === "player-2" || props.forPlayer === "player-3"
-            ? "padding-right: 0.5rem;"
-            : "padding-left: 0.5rem;"}
+            ? "padding-right: 0.125rem; text-align: right; order: 1;"
+            : ""};
         `}
       >
         {numberWithCommas(props.betAmount)}
