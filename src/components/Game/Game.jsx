a1cc66de50879b1158fc5ad1fd367b0495f1cc5c
Norbert Dragán <norbert.dragan@gmail.com>
2019-07-04 16:31:28 +0800

    Add Reconnect button

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 import WebSocket from "./WebSocket";
@@ -18,6 +18,26 @@ const Game = () => {
   const dispatch = useContext(DispatchContext);
   const state = useContext(StateContext);
 
+  const [webSocketKeys, setWebSocketKeys] = useState({
+    dcv: 1,
+    bvv: 2,
+    player1: 3,
+    player2: 4
+  });
+
+  const [reconnect, setReconnect] = useState(0);
+
+  // TODO: This is not a very elegant solution to refresh the Websocket when they disconenct.
+  useEffect(() => {
+    Object.keys(webSocketKeys).map(node => {
+      state.connection[node] == "Disconnected" &&
+        setWebSocketKeys({
+          ...webSocketKeys,
+          [node]: Math.random()
+        });
+    });
+  }, [reconnect]);
+
   return (
     <div>
       <div
@@ -28,50 +48,46 @@ const Game = () => {
       >
         {state.gameStarted === false && (
           <Button
-            label="Starts"
-            onClick={
-              () =>
-                // GameAPI.sendMessage({ method: "game" }, "dcv", state, dispatch)
-                GameAPI.sendMessage(
-                  {
-                    method: "replay",
-                    action: "round_betting",
-                    playerid: 1,
-                    round: 1,
-                    pot: 4000000,
-                    actions: [5, 3, 3],
-                    possibilities: [0, 1, 2, 3, 4, 5, 6, 7],
-                    min_amount: 0,
-                    gui_playerID: 1
-                  },
-                  "player2",
-                  state,
-                  dispatch
-                )
-              // GameAPI.toggleControls(dispatch)
+            label="Start"
+            onClick={() =>
+              GameAPI.sendMessage({ method: "game" }, "dcv", state, dispatch)
             }
           />
         )}
       </div>
+      <div
+        css={css`
+          position: absolute;
+          z-index: 5;
+          left: 0;
+          top: 2.7rem;
+        `}
+      >
+        <Button label="Reconnect" onClick={() => setReconnect(Math.random())} />
+      </div>
       <WebSocket
         nodeName="dcv"
         server={SOCKET_URL_DCV}
-        message={state.message.bvv}
+        message={state.message.dcv}
+        key={webSocketKeys.dcv}
       />
       <WebSocket
         nodeName="bvv"
         server={SOCKET_URL_BVV}
         message={state.message.bvv}
+        key={webSocketKeys.bvv}
       />
       <WebSocket
         nodeName="player1"
         server={SOCKET_URL_PLAYER1}
         message={state.message.player1}
+        key={webSocketKeys.player1}
       />
       <WebSocket
         nodeName="player2"
-        server={SOCKET_URL_ECHO}
+        server={SOCKET_URL_PLAYER2}
         message={state.message.player2}
+        key={webSocketKeys.player2}
       />
     </div>
   );
