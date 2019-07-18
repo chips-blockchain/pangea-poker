98243c38178d9b05faf4c7e0a4c33b178a613fe7
Norbert Dragán <norbert.dragan@gmail.com>
2019-07-18 11:23:40 +0200

    Convert App to a functional component

--- src/index.js
+++ src/index.js
@@ -6,40 +6,38 @@ import "normalize.css";
 import NoticiaBold from "./fonts/NoticiaText-Bold.ttf";
 import PTSansBold from "./fonts/PT_Sans-Web-Bold.ttf";
 
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Global
-          styles={css`
-            @font-face {
-              font-family: "Noticia Text";
-              font-style: bold;
-              font-weight: 700;
-              font-display: swap;
-              src: local("Noticia Text Bold"), local("NoticiaText-Bold"),
-                url(${NoticiaBold}) format("woff2");
-            }
-            @font-face {
-              font-family: "PT Sans";
-              font-style: bold;
-              font-weight: 700;
-              font-display: swap;
-              src: local("PT Sans Bold"), local("PT_Sans-Web-Bold"),
-                url(${PTSansBold}) format("woff2");
-            }
-            * {
-              -webkit-font-smoothing: antialiased;
-              -moz-osx-font-smoothing: grayscale;
-              font-family: "Noticia Text";
-            }
-          `}
-        />
-        <Main />
-      </React.Fragment>
-    );
-  }
-}
+const App = () => {
+  return (
+    <React.Fragment>
+      <Global
+        styles={css`
+          @font-face {
+            font-family: "Noticia Text";
+            font-style: bold;
+            font-weight: 700;
+            font-display: swap;
+            src: local("Noticia Text Bold"), local("NoticiaText-Bold"),
+              url(${NoticiaBold}) format("woff2");
+          }
+          @font-face {
+            font-family: "PT Sans";
+            font-style: bold;
+            font-weight: 700;
+            font-display: swap;
+            src: local("PT Sans Bold"), local("PT_Sans-Web-Bold"),
+              url(${PTSansBold}) format("woff2");
+          }
+          * {
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            font-family: "Noticia Text";
+          }
+        `}
+      />
+      <Main />
+    </React.Fragment>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
