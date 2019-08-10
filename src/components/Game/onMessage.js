2d9abfb7c08d25d83fa966d6ef746cfefd1a47d0
sg777 <sarath.ginjupalli89@gmail.com>
2019-08-10 12:01:49 +0530

    pushed the action information on to the gui

--- src/components/Game/onMessage.js
+++ src/components/Game/onMessage.js
@@ -303,14 +303,15 @@ export const onMessage_player1 = (message, state, dispatch) => {
 
     default:
       switch (message["action"]) {
+	  	/* Here we receive the other players action information*/
         case "check":
         case "call":
         case "raise":
         case "fold":
         case "allin":
-          message["gui_playerID"] = 0;
-          sendMessage(message, "dcv", state, dispatch);
-          break;
+          //message["gui_playerID"] = 0;
+          //sendMessage(message, "dcv", state, dispatch);
+         break;
 
         default:
           sendMessage(message, "dcv", state, dispatch);
@@ -430,13 +431,14 @@ export const onMessage_player2 = (message, state, dispatch) => {
 
     default:
       switch (message["action"]) {
+	  	/* Here we receive the other players action information*/
         case "check":
         case "call":
         case "raise":
         case "fold":
         case "allin":
-          message["gui_playerID"] = 1;
-          sendMessage(message, "dcv", state, dispatch);
+          //message["gui_playerID"] = 1;
+          //sendMessage(message, "dcv", state, dispatch);
           break;
 
         default:
