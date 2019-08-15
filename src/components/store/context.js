b34f1f65f9ffeb388abbcbacc9984f68e52fc3f6
Norbert Dragán <norbert.dragan@gmail.com>
2019-08-15 13:54:15 +0200

    Refactor context

--- src/components/store/context.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-export const StateContext = createContext();
-export const DispatchContext = createContext();
---
+++ src/components/store/context.ts
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+export const StateContext = createContext(null, null);
+export const DispatchContext = createContext(null, null);
