1188e598859981dffe4f684ddf552428e5e8b943
Norbert Dragán <norbert.dragan@gmail.com>
2019-08-23 17:02:21 +0200

    Refactor TestStateProvider

---
+++ src/lib/TestStateProvider.tsx
@@ -0,0 +1,18 @@
+import { useReducer } from "react";
+import { StateContext, DispatchContext } from "../store/context";
+import initialState, { IState } from "../store/initialState";
+import reducer from "../store/reducer";
+
+const TestStateProvider = ({ children }) => {
+  const [state, dispatch]: [IState, Function] = useReducer(
+    reducer,
+    initialState
+  );
+  return (
+    <DispatchContext.Provider value={dispatch}>
+      <StateContext.Provider value={state}>{children}</StateContext.Provider>
+    </DispatchContext.Provider>
+  );
+};
+
+export default TestStateProvider;
