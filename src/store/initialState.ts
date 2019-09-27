8b646cfe1982735edb0a1d6c737e0319c430ef9a
Norbert Dragán <norbert.dragan@gmail.com>
2019-09-27 14:58:20 +0200

    Enable developer mode

--- src/store/initialState.ts
+++ src/store/initialState.ts
@@ -59,7 +59,7 @@ const initialState: IState = {
   // Cards of the user
   holeCards: [],
   // Wether the app should run in developer mode
-  isDeveloperMode: false,
+  isDeveloperMode: true,
   // Wether the Startup Modal shows at the beginning of the game
   isStartupModal: true,
   // Object that stores the last action so we can dispaly it on the UI
