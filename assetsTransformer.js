34ee13f3135199a12a7b5df352c0b18ec423b972
Norbert Dragán <norbert.dragan@gmail.com>
2019-10-25 18:35:56 +0200

    Configure Jest with Enzyme

--- assetsTransformer.js
+++
@@ -1,9 +0,0 @@
-// Transform assets so Jest runs properly
-
-const path = require("path");
-
-module.exports = {
-  process(src, filename, config, options) {
-    return "module.exports = " + JSON.stringify(path.basename(filename)) + ";";
-  }
-};
