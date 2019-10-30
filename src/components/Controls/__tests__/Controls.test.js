83653580a435d59a51974ab6744019a3f49af14f
Norbert Dragán <norbert.dragan@gmail.com>
2019-10-30 15:40:17 +0100

    Refactor Raise button hide unit tests

--- src/components/Controls/__tests__/Controls.test.js
+++ src/components/Controls/__tests__/Controls.test.js
@@ -29,14 +29,18 @@ describe("Controls", () => {
   test("does not show the Raise button when Call is already All-In", () => {
     // The player's stack is 200
     const state = testState;
+    // Call is equal to the stack
     state.toCall = 200;
-    const wrapper = buildWrapper(state);
+    let wrapper = buildWrapper(state);
 
     expect(wrapper.find(`[label="Raise to"]`)).toHaveLength(0);
     expect(wrapper.find(`[label="All-In"]`)).toHaveLength(0);
     expect(wrapper.find(`[label="Bet"]`)).toHaveLength(0);
 
+    // Call is larger than the stack
     state.toCall = 300;
+    wrapper = buildWrapper(state);
+
     expect(wrapper.find(`[label="Raise to"]`)).toHaveLength(0);
     expect(wrapper.find(`[label="All-In"]`)).toHaveLength(0);
     expect(wrapper.find(`[label="Bet"]`)).toHaveLength(0);
