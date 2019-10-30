38fb92cc34ac2c0405647697feaee8d69b699e9d
Norbert Dragán <norbert.dragan@gmail.com>
2019-10-30 15:47:00 +0100

    Add test coverage for Call and Raise button labels

--- src/components/Controls/__tests__/Controls.test.js
+++ src/components/Controls/__tests__/Controls.test.js
@@ -26,6 +26,43 @@ describe("Controls", () => {
     expect(wrapper.find("Slider")).toHaveLength(1);
   });
 
+  test("shows the Call amount on the Call button when raise is not All-In", () => {
+    const state = testState;
+    state.toCall = 100;
+    const wrapper = buildWrapper(state);
+
+    expect(wrapper.find(`[label="Call"]`)).toHaveLength(1);
+    expect(wrapper.find(`[label="Call"]`).text()).toBe("Call 100");
+  });
+
+  test("shows the Raise amount on the Raise button when raise is not All-In", () => {
+    const state = testState;
+    state.minRaiseTo = 100;
+    const wrapper = buildWrapper(state);
+
+    expect(wrapper.find(`[label="Raise to"]`)).toHaveLength(1);
+    expect(wrapper.find(`[label="Raise to"]`).text()).toBe("Raise to 100");
+  });
+
+  test("shows All-In button instead of Raise button when raise would be an all in", () => {
+    // The player's stack is 200
+    const state = testState;
+
+    // Minimum raise is more than the player's stack
+    state.minRaiseTo = 300;
+    let wrapper = buildWrapper(state);
+
+    expect(wrapper.find(`[label="Raise to"]`)).toHaveLength(0);
+    expect(wrapper.find(`[label="All-In"]`)).toHaveLength(1);
+
+    // Minimum raise is equals the player's stack
+    state.minRaiseTo = 200;
+    wrapper = buildWrapper(state);
+
+    expect(wrapper.find(`[label="Raise to"]`)).toHaveLength(0);
+    expect(wrapper.find(`[label="All-In"]`)).toHaveLength(1);
+  });
+
   test("does not show the Raise button when Call is already All-In", () => {
     // The player's stack is 200
     const state = testState;
