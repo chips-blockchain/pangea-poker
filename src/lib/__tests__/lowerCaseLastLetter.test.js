2dfb55b764fd8964096e4d0bf45a6c00482c7224
Norbert Dragán <norbert.dragan@gmail.com>
2019-11-25 17:27:00 +0100

    Add test coverage for when last charachter is not a letter

--- src/lib/__tests__/lowerCaseLastLetter.test.js
+++ src/lib/__tests__/lowerCaseLastLetter.test.js
@@ -6,4 +6,16 @@ describe("LowerCases the last letter of a string", () => {
     expect(lowerCaseLastLetter("A")).toBe("a");
     expect(lowerCaseLastLetter("10S")).toBe("10s");
   });
+
+  test("Throws error if last letter is not a letter", () => {
+    expect(() => lowerCaseLastLetter("A9")).toThrow(
+      "The last character is 9 which is not a letter."
+    );
+    expect(() => lowerCaseLastLetter("A,")).toThrow(
+      "The last character is , which is not a letter."
+    );
+    expect(() => lowerCaseLastLetter("")).toThrow(
+      "Empty strings don't have last letter."
+    );
+  });
 });
--- src/lib/lowerCaseLastLetter.ts
+++ src/lib/lowerCaseLastLetter.ts
@@ -1,5 +1,14 @@
-const lowerCaseLastLetter = (string: string) =>
-  string.slice(0, string.length - 1) +
-  string.slice(string.length - 1).toLowerCase();
+const lowerCaseLastLetter = (string: string) => {
+  if (string.length === 0) {
+    throw new Error("Empty strings don't have last letter.");
+  }
+  const lastCharacter = string.slice(string.length - 1);
+  if (lastCharacter.toUpperCase() != lastCharacter.toLowerCase()) {
+    return string.slice(0, string.length - 1) + lastCharacter.toLowerCase();
+  } else
+    throw new Error(
+      `The last character is ${lastCharacter} which is not a letter.`
+    );
+};
 
 export default lowerCaseLastLetter;
