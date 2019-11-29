f212785cf3c42d79cdca4e8b69c833212797f760
Norbert Dragán <norbert.dragan@gmail.com>
2019-11-29 13:26:53 +0100

    Match timeStamp range instead of stubbing it

--- src/store/__tests__/reducer.test.js
+++ src/store/__tests__/reducer.test.js
@@ -4,21 +4,39 @@ import state from "../initialState";
 import initialState from "../initialState";
 
 describe("reducer", () => {
+  expect.extend({
+    toBeWithinRange(received, floor, ceiling) {
+      const pass = received >= floor && received <= ceiling;
+      if (pass) {
+        return {
+          message: () =>
+            `expected ${received} not to be within range ${floor} - ${ceiling}`,
+          pass: true
+        };
+      } else {
+        return {
+          message: () =>
+            `expected ${received} to be within range ${floor} - ${ceiling}`,
+          pass: false
+        };
+      }
+    }
+  });
+
   test("handles addToHandHistory", () => {
     const action = {
       payload: "Player 1 has joined the table",
       type: "addToHandHistory"
     };
 
-    const realDateNow = Date.now.bind(global.Date);
-    const dateNowStub = jest.fn(() => 1530518207007);
-    global.Date.now = dateNowStub;
-
     expect(reducer(state, action)).toEqual({
       ...initialState,
-      handHistory: [{ action: action.payload, timeStamp: dateNowStub() }]
+      handHistory: [
+        {
+          action: action.payload,
+          timeStamp: expect.toBeWithinRange(Date.now() - 10, Date.now() + 10)
+        }
+      ]
     });
-
-    global.Date.now = realDateNow;
   });
 });
