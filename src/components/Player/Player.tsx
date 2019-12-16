e9babdd7e0e95c7f215d821caa84096f85bb0627
Norbert Dragán <norbert.dragan@gmail.com>
2019-12-16 13:00:30 +0100

    Add test for joining the table

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -214,6 +214,7 @@ const Player: React.FunctionComponent<IProps> = ({
           setSeatMessage("SITTING...");
         }
       }}
+      data-test={`player-widget-${seat}`}
     >
       {cardsDealt && showCards && hasCards && (
         <CardsWrapper>
--- src/components/Player/__tests__/Player.test.js
+++ src/components/Player/__tests__/Player.test.js
@@ -2,27 +2,80 @@ import React from "react";
 import { mount } from "enzyme";
 import { StateContext, DispatchContext } from "../../../store/context";
 import testState from "../../../store/testState";
+import * as actions from "../../../store/actions";
 import Player from "../Player";
 
 const dispatch = jest.fn();
 
-const buildWrapper = (dispatch, state) => {
+const buildWrapper = (props, dispatch, state) => {
   return mount(
     <DispatchContext.Provider value={dispatch}>
       <StateContext.Provider value={state}>
-        <Player connected chips={1000} seat="player1" isActive />
+        <Player
+          chips={props.chips}
+          connected={props.connected}
+          hasCards={props.hasCards}
+          isActive={props.isActive}
+          playerCards={props.playerCards}
+          seat={props.seat}
+          showCards={props.showCards}
+          winner={props.winner}
+        />
       </StateContext.Provider>
     </DispatchContext.Provider>
   );
 };
 
+// Props
+const defaultProps = {
+  chips: 200,
+  connected: true,
+  hasCards: true,
+  isActive: true,
+  playerCards: ["Ac", "Ad"],
+  seat: "player1",
+  showCards: false,
+  winner: "player1"
+};
+
+const { seat } = defaultProps;
+
+// Action Spies
+jest.spyOn(actions, "playerJoin");
+jest.spyOn(actions, "playerJoin");
+const { playerJoin, setSeatMessage } = actions;
+
 describe("Player", () => {
+  test("joins when not conencted and clicked", () => {
+    const state = {
+      ...testState,
+      userSeat: "player1"
+    };
+    const wrapper = buildWrapper(
+      { ...defaultProps, connected: false },
+      dispatch,
+      state
+    );
+
+    const playerWidget = wrapper.find(
+      `div[data-test="player-widget-${defaultProps.seat}"]`
+    );
+
+    expect(playerWidget.exists()).toEqual(true);
+
+    playerWidget.simulate("click");
+
+    expect(playerJoin).toHaveBeenCalled();
+    expect(playerJoin).toHaveBeenCalledTimes(1);
+    expect(playerJoin).toHaveBeenCalledWith(seat, state, dispatch);
+  });
+
   test("is highlighted when active", () => {
     const state = {
       ...testState,
       userSeat: "player1"
     };
-    const wrapper = buildWrapper(dispatch, state);
+    const wrapper = buildWrapper(defaultProps, dispatch, state);
 
     expect(wrapper.find(`[data-test="player-highlight"]`).exists()).toEqual(
       true
@@ -34,7 +87,7 @@ describe("Player", () => {
       ...testState,
       userSeat: "player1"
     };
-    const wrapper = buildWrapper(dispatch, state);
+    const wrapper = buildWrapper(defaultProps, dispatch, state);
 
     expect(wrapper.find(`[data-test="player-timer-bar"]`).exists()).toEqual(
       true
