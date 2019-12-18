64ecbf6f17360da75db4ef3c9b061b4c00134eb9
Norbert Dragán <norbert.dragan@gmail.com>
2019-12-18 13:19:55 +0100

    Separate Modal component to make it more reusable

--- src/components/StartupModal/StartupModal.tsx
+++ src/components/StartupModal/StartupModal.tsx
@@ -10,12 +10,12 @@ import {
   setUserSeat
 } from "../../store/actions";
 import { IState } from "../../store/initialState";
-import { connect } from "net";
+import Modal from "./Modal";
 
 // This is the modal that appears at the startup and prompts the user to type in the
 // IP addresses for the nodes. Used for testing purposes only.
 
-const Modal = () => {
+const StartupModal = () => {
   const dispatch: Function = useContext(DispatchContext);
   const state: IState = useContext(StateContext);
 
@@ -116,141 +116,84 @@ const Modal = () => {
   }, [nodes]);
 
   return (
-    <div
-      css={css`
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        height: 37.5rem;
-        width: 50rem;
-        z-index: 999;
-      `}
+    <Modal
+      title="Please enter the node addresses"
+      isDisabled={!canSetNodes}
+      handleSubmit={handleSubmit}
     >
-      <div
-        css={css`
-          position: absolute;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          background-color: ${theme.moon.colors.background};
-          opacity: 0.5;
-        `}
-      />
-      <div
-        css={css`
-          color: ${theme.moon.colors.text};
-          background-color: ${theme.moon.colors.background};
-          left: 50%;
-          margin: auto;
-          transform: translate(-50%, -50%);
-          text-align: center;
-          top: 50%;
-          padding: 2rem 5.5rem;
-          position: absolute;
-          right: 0;
-        `}
-      >
-        <form>
-          <h1
-            css={css`
-              font-size: 1rem;
-            `}
-          >
-            Please enter the node addresses
-          </h1>
-          <div>
-            <Button
-              small
-              label="Dealer"
-              onClick={e => {
-                hanldeTabClick(e, "dealer");
-                process.env && setDevNodeTypes("dealer");
-              }}
-            />
-            <Button
-              small
-              label="Player1"
-              onClick={e => {
-                hanldeTabClick(e, "player1");
-                process.env && setDevNodeTypes("player1");
-              }}
-            />
-            <Button
-              small
-              label="Player2"
-              onClick={e => {
-                hanldeTabClick(e, "player2");
-                process.env && setDevNodeTypes("player2");
-              }}
-            />
-          </div>
-          <div id="Dealer" />
-          {nodesToInput
-            .filter(node => node.id === nodeType)
-            .map((node, key) => {
-              return (
-                <div key={key}>
-                  {/* Label*/}
-                  <div
-                    css={css`
-                      color: ${theme.moon.colors.text};
-                      padding: 1rem 0 0.5rem 0;
-                      font-size: 0.875rem;
-                    `}
-                  >
-                    {node.name}
-                  </div>
-                  {/* Input field */}
-                  <input
-                    css={css`
-                      background: none;
-                      border: 1px solid ${theme.moon.colors.primary};
-                      color: white;
-                      font-family: sans-serif;
-                      font-weight: 500;
-                      text-align: center;
-                      padding: 0.5rem 0.25rem;
-                      width: 100%;
-
-                      &:focus {
-                        border: 1px solid ${theme.moon.colors.accent};
-                      }
-                    `}
-                    name={node.name}
-                    placeholder={`192.168.101.234`}
-                    defaultValue={process.env ? node.devAddress : ""}
-                    onChange={e => {
-                      setNodes({
-                        ...nodes,
-                        [node.name]: e.target.value
-                      });
-                    }}
-                  />
-                </div>
-              );
-            })}
-          <div
-            css={css`
-              text-align: center;
-              padding-top: 2rem;
-            `}
-          >
-            <Button
-              label="Set Nodes"
-              disabled={!canSetNodes}
-              onClick={e => {
-                e.preventDefault();
-                handleSubmit();
-              }}
-            />
-          </div>
-        </form>
+      <div>
+        <Button
+          small
+          label="Dealer"
+          onClick={e => {
+            hanldeTabClick(e, "dealer");
+            process.env && setDevNodeTypes("dealer");
+          }}
+        />
+        <Button
+          small
+          label="Player1"
+          onClick={e => {
+            hanldeTabClick(e, "player1");
+            process.env && setDevNodeTypes("player1");
+          }}
+        />
+        <Button
+          small
+          label="Player2"
+          onClick={e => {
+            hanldeTabClick(e, "player2");
+            process.env && setDevNodeTypes("player2");
+          }}
+        />
       </div>
-    </div>
+      <div id="Dealer" />
+      {nodesToInput
+        .filter(node => node.id === nodeType)
+        .map((node, key) => {
+          return (
+            <div key={key}>
+              {/* Label*/}
+              <div
+                css={css`
+                  color: ${theme.moon.colors.text};
+                  padding: 1rem 0 0.5rem 0;
+                  font-size: 0.875rem;
+                `}
+              >
+                {node.name}
+              </div>
+              {/* Input field */}
+              <input
+                css={css`
+                  background: none;
+                  border: 1px solid ${theme.moon.colors.primary};
+                  color: white;
+                  font-family: sans-serif;
+                  font-weight: 500;
+                  text-align: center;
+                  padding: 0.5rem 0.25rem;
+                  width: 100%;
+
+                  &:focus {
+                    border: 1px solid ${theme.moon.colors.accent};
+                  }
+                `}
+                name={node.name}
+                placeholder={`192.168.101.234`}
+                defaultValue={process.env ? node.devAddress : ""}
+                onChange={e => {
+                  setNodes({
+                    ...nodes,
+                    [node.name]: e.target.value
+                  });
+                }}
+              />
+            </div>
+          );
+        })}
+    </Modal>
   );
 };
 
-export default Modal;
+export default StartupModal;
