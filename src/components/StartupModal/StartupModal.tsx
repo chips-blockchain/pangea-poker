2cc3834503b434b556716f9d851905758315f6b4
Norbert Dragán <norbert.dragan@gmail.com>
2020-01-07 17:26:05 +0100

    Separate Tabs style

--- src/components/StartupModal/StartupModal.tsx
+++ src/components/StartupModal/StartupModal.tsx
@@ -4,75 +4,15 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import Modal from "./Modal";
 import CustomIP from "./CustomIP";
 import TableSelect from "./TableSelect";
-import theme from "../../styles/theme";
 import { IState } from "../../store/initialState";
+
 // This is the modal that appears at the startup and let's the user to join a table
 
-const tabsStyle = css`
-  .react-tabs {
-    -webkit-tap-highlight-color: transparent;
-  }
-
-  .react-tabs__tab-list {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+const modalTopTabs = css`
+  & > .react-tabs__tab-list {
     position: absolute;
     top: 0;
     left: 0;
-    width: 100%;
-    margin: 0 0 10px;
-    padding: 0;
-  }
-
-  .react-tabs__tab {
-    display: inline-block;
-    border-top: 2px solid transparent;
-    position: relative;
-    list-style: none;
-    padding: 6px 12px;
-    cursor: pointer;
-  }
-
-  .react-tabs__tab:hover:not(.react-tabs__tab--selected) {
-    color: ${theme.moon.colors.accent};
-  }
-
-  .react-tabs__tab--selected {
-    border-top: 2px solid ${theme.moon.colors.primaryLight};
-    background: linear-gradient(
-      180deg,
-      rgba(109, 171, 171, 0.2) 0%,
-      rgba(109, 171, 171, 0) 100%
-    );
-  }
-
-  .react-tabs__tab--disabled {
-    color: GrayText;
-    cursor: default;
-  }
-
-  .react-tabs__tab:focus {
-    box-shadow: 0 0 5px ${theme.moon.colors.primaryLight};
-    border-color: ${theme.moon.colors.primaryLight};
-    outline: none;
-  }
-
-  .react-tabs__tab:focus:after {
-    content: "";
-    position: absolute;
-    height: 2px;
-    left: -4px;
-    right: -4px;
-    bottom: -2px;
-    background: ${theme.moon.colors.primary};
-  }
-
-  .react-tabs__tab-panel {
-    display: none;
-  }
-
-  .react-tabs__tab-panel--selected {
-    display: block;
   }
 `;
 
@@ -87,8 +27,11 @@ const StartupModal: React.FunctionComponent<IProps> = ({
 }): React.ReactElement => {
   return (
     <Modal>
-      {/* TabsWrapper */}
-      <Tabs css={tabsStyle}>
+      <Tabs
+        css={css`
+          ${modalTopTabs}
+        `}
+      >
         <TabList>
           <Tab>Table List</Tab>
           <Tab>Custom IP</Tab>
