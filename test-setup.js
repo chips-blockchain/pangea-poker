9eb410bd157c603820e141216b3eb7b0c5a07ec6
Norbert Dragán <norbert.dragan@gmail.com>
2020-01-17 16:16:40 +0800

    Stub unsupported media operations

--- test-setup.js
+++ test-setup.js
@@ -2,3 +2,12 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+beforeEach(() => {
+  window.HTMLMediaElement.prototype.play = () => {
+    /* do nothing */
+  };
+  window.HTMLMediaElement.prototype.load = () => {
+    /* do nothing */
+  };
+});
