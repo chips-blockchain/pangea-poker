31ccae1602e894471b4d366072cacab521c7ce35
Norbert Dragán <norbert.dragan@gmail.com>
2020-01-31 13:02:43 +0800

    Add function to convert arrays to sentences

---
+++ src/lib/__tests__/arrayListToSentence.test.js
@@ -0,0 +1,58 @@
+import arrayListToSentence from "../arrayListToSentence";
+
+console.error = jest.fn();
+
+describe("Turns an array into a sentence string", () => {
+  test("Returns the correct string with two string elements", () => {
+    const array = ["Ad", "Kc"];
+    expect(arrayListToSentence(array)).toBe("Ad and Kc");
+  });
+  test("Returns the correct string with three string elements", () => {
+    const array = ["Ad", "Kc", "5d"];
+    expect(arrayListToSentence(array)).toBe("Ad, Kc and 5d");
+  });
+  test("Returns the correct string with both mixed and string elements", () => {
+    const array = ["Ad", 4, "5d", "Qd"];
+    expect(arrayListToSentence(array)).toBe("Ad, 4, 5d and Qd");
+  });
+  test("Returns the correct string with number elements", () => {
+    const array = [5, 6, 7];
+    expect(arrayListToSentence(array)).toBe("5, 6 and 7");
+  });
+  test("Returns the correct string with one element", () => {
+    const array = ["Ad"];
+    expect(arrayListToSentence(array)).toBe("Ad");
+  });
+  test("Logs an error to the console when the array is empty", () => {
+    const array = [];
+    arrayListToSentence(array);
+    expect(console.error).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(
+      "Can't convert empty array to a sentence."
+    );
+  });
+  test("Logs an error to the console when the array has undefined in it", () => {
+    const array = [undefined, "4"];
+    console.log(arrayListToSentence(array));
+    expect(console.error).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(
+      "Can only convert an array containing string or number elements"
+    );
+  });
+  test("Logs an error to the console when the array has null in it", () => {
+    const array = [null, "4"];
+    console.log(arrayListToSentence(array));
+    expect(console.error).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(
+      "Can only convert an array containing string or number elements"
+    );
+  });
+  test("Logs an error to the console when the array has boolean in it", () => {
+    const array = [true, "4"];
+    console.log(arrayListToSentence(array));
+    expect(console.error).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(
+      "Can only convert an array containing string or number elements"
+    );
+  });
+});
---
+++ src/lib/arrayListToSentence.ts
@@ -0,0 +1,18 @@
+const isStringOrNumber = (element): boolean =>
+  typeof element === "string" || typeof element === "number";
+
+const arrayListToSentence = (array: string[] | number[]): string => {
+  if (array.length > 0) {
+    if (array.every(isStringOrNumber))
+      return array
+        .toString()
+        .replace(/,/g, ", ")
+        .replace(/, (?!.*,)/g, " and ");
+    else
+      console.error(
+        "Can only convert an array containing string or number elements"
+      );
+  } else console.error("Can't convert empty array to a sentence.");
+};
+
+export default arrayListToSentence;
