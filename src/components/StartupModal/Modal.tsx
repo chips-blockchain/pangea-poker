10b084f7f117ba7d8ecbe970160f7be2ab8462ef
Norbert Dragán <norbert.dragan@gmail.com>
2020-02-06 10:32:31 +0800

    Separate modal style to a constant to improve readability

--- src/components/StartupModal/Modal.tsx
+++ src/components/StartupModal/Modal.tsx
@@ -1,33 +1,32 @@
 import React from "react";
 import ReactModal from "react-modal";
 
+const modalStyle = {
+  content: {
+    backgroundColor: "var(--background)",
+    border: 0,
+    color: "var(--text)",
+    left: "50%",
+    opacity: 1,
+    padding: "2rem 2rem",
+    textAlign: "center",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+    minWidth: "17rem",
+    minHeight: "22.5rem",
+    zIndex: 1000
+  },
+  overlay: {
+    backgroundColor: "#0000007F",
+    position: "absolute",
+    height: "37.5rem",
+    width: "50rem"
+  }
+};
+
 const Modal: React.FunctionComponent = ({ children }) => {
   return (
-    <ReactModal
-      isOpen={true}
-      style={{
-        content: {
-          backgroundColor: "var(--background)",
-          border: 0,
-          color: "var(--text)",
-          left: "50%",
-          opacity: 1,
-          padding: "2rem 2rem",
-          textAlign: "center",
-          top: "50%",
-          transform: "translate(-50%, -50%)",
-          minWidth: "17rem",
-          minHeight: "22.5rem",
-          zIndex: 1000
-        },
-        overlay: {
-          backgroundColor: "#0000007F",
-          position: "absolute",
-          height: "37.5rem",
-          width: "50rem"
-        }
-      }}
-    >
+    <ReactModal isOpen={true} style={modalStyle}>
       {children}
     </ReactModal>
   );
