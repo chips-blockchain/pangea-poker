1946db3c46666140c825cfcd87ba5b5ecca68cae
Norbert Dragán <norbert.dragan@gmail.com>
2020-02-06 12:23:59 +0800

    Refactor modal tabs and content to make it more reusable

--- src/components/StartupModal/StartupModal.tsx
+++ src/components/StartupModal/StartupModal.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { css } from "@emotion/core";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import Modal from "./Modal";
 import CustomIP from "./CustomIP";
 import TableSelect from "./TableSelect";
@@ -13,35 +11,20 @@ interface IProps {
   state: IState;
 }
 
-const modalTopTabs = css`
-  & > .react-tabs__tab-list {
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-`;
-
 const StartupModal: React.FunctionComponent<IProps> = ({ dispatch, state }) => {
   return (
-    <Modal>
-      <Tabs
-        css={css`
-          ${modalTopTabs}
-        `}
-      >
-        <TabList>
-          <Tab>Table List</Tab>
-          <Tab>Custom IP</Tab>
-        </TabList>
-
-        <TabPanel>
-          <TableSelect dispatch={dispatch} state={state} />
-        </TabPanel>
-        <TabPanel>
-          <CustomIP />
-        </TabPanel>
-      </Tabs>
-    </Modal>
+    <Modal
+      tabs={[
+        {
+          title: "Table List",
+          content: <TableSelect dispatch={dispatch} state={state} />
+        },
+        {
+          title: "Custom IP",
+          content: <CustomIP />
+        }
+      ]}
+    />
   );
 };
 
