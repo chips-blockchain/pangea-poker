979e25b0ced1232961c67323bea10341b7d02f52
Norbert Dragán <norbert.dragan@gmail.com>
2020-02-11 12:43:59 +0800

    Remove redundant control flow

--- src/lib/balanceWithDecimals.ts
+++ src/lib/balanceWithDecimals.ts
@@ -1,9 +1,7 @@
 // Displays the balance with 8 decimals
 
 const displayBalanceDecimals = (balance: number): string => {
-  if (typeof balance === "number") {
-    return (Math.round(balance * 100000000) / 100000000).toFixed(8);
-  } else console.error(`Balance is not a number, it's ${typeof balance}.`);
+  return (Math.round(balance * 100000000) / 100000000).toFixed(8);
 };
 
 export default displayBalanceDecimals;
