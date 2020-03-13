562729ef90c567a3a4f63a6966f245f6e72b2304
Norbert Dragán <norbert.dragan@gmail.com>
2020-03-13 13:33:33 +0100

    Remove InputWithButton import

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,4 @@
 import Dropdown from "./Dropdown";
 import Input from "./Input";
-import InputWithButton from "./InputWithButton";
 
-export { Dropdown, Input, InputWithButton };
+export { Dropdown, Input };
