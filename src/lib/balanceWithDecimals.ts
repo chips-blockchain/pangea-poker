aabe99795c4c206acc2a32a2105cff5adba8d051
Norbert Dragán <norbert.dragan@gmail.com>
2020-03-17 17:28:58 +0100

    Fix Deposit types

--- src/lib/balanceWithDecimals.ts
+++ src/lib/balanceWithDecimals.ts
@@ -1,7 +1,7 @@
 // Displays the balance with 8 decimals
 
-const displayBalanceDecimals = (balance: number): string => {
-  return (Math.round(balance * 100000000) / 100000000).toFixed(8);
+const displayBalanceDecimals = (balance: number | string): string => {
+  return (Math.round(Number(balance) * 100000000) / 100000000).toFixed(8);
 };
 
 export default displayBalanceDecimals;
