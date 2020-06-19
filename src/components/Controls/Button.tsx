92bf6db617df779275c3d5e7cbba82466711b145
piggydoughnut <drmikhailova@gmail.com>
2020-06-19 21:38:06 +1200

    separated css from button component

--- src/components/Controls/Button.tsx
+++ src/components/Controls/Button.tsx
@@ -1,7 +1,5 @@
-import { css, SerializedStyles } from "@emotion/core";
-import styled from "@emotion/styled";
 import numberWithCommas from "../../lib/numberWithCommas";
-
+import {ButtonStyle, ButtonInnerWrapper, highlightedButtonStyle} from "./css/ButtonStyle";
 // This component renders a Button
 
 interface IProps {
@@ -27,77 +25,6 @@ const Button: React.FunctionComponent<IProps> = ({
   small,
   testId
 }) => {
-  const ButtonStyle = styled.button`
-    background: var(--color-background);
-    border: 0.0625rem solid var(--color-primary);
-    border-radius: 0.125rem;
-    color: var(--color-text);
-    font-size: ${small ? ".75rem" : "1rem"};
-    font-weight: 700;
-    height: ${small ? "1.75rem" : "2.5rem"};
-    margin: 0.5rem;
-    opacity: ${disabled && "0.5"};
-    line-height: 1rem;
-    transition: 0.1s ease;
-    width: ${small ? "3.625rem" : "7.5rem"};
-
-    ${disabled && `cursor: not-allowed;`}
-    ${!disabled &&
-      `
-&:hover {
-  color: var(--color-accent);
-  border-color: var(--color-accent);
-  cursor: pointer;
-}
-&:focus {
-  color: var(--color-accent);
-  border-color: var(--color-accent);
-}
-&:active {
-  background: var(--color-accent);
-  color: var(--color-background);
-}
-
-`}
-  `;
-
-  const highlightedButtonStyle = css`
-    background: var(--color-primaryLight);
-    border-color: var(--color-primaryLight);
-    color: var(--color-background);
-
-    ${!disabled &&
-      `
-  &:hover {
-      background: var(--color-accent);
-      border-color: var(--color-accent);
-      color: var(--color-background);
-    }`}
-
-    &:active {
-      border-color: var(--color-primary);
-    }
-  `;
-
-  const ButtonInnerWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    font-family: var(--font-family-secondary);
-    text-align: center;
-    justify-content: center;
-    /* Crop line height */
-    ${amount &&
-      `
-          &:before {
-            content: "";
-            display: block;
-            height: 0;
-            width: 0;
-            margin-top: 0.75rem;
-          }
-         
-          `}
-  `;
   return (
     <ButtonStyle
       css={css`
@@ -107,9 +34,10 @@ const Button: React.FunctionComponent<IProps> = ({
       onClick={onClick}
       data-testid={testId}
       disabled={disabled}
+      small={small}
       type={isSubmit ? "submit" : "button"}
     >
-      <ButtonInnerWrapper>
+      <ButtonInnerWrapper amount={amount}>
         {label} <br />
         {/* Show an amount for the Raise and Call buttons */}
         {amount && numberWithCommas(amount)}
---
+++ src/components/Controls/css/ButtonStyle.tsx
@@ -0,0 +1,77 @@
+import { css, SerializedStyles } from "@emotion/core";
+import styled from "@emotion/styled";
+
+
+const effs = ` 
+&:hover {
+  color: var(--color-accent);
+  border-color: var(--color-accent);
+  cursor: pointer;
+};
+&:focus {
+  color: var(--color-accent);
+  border-color: var(--color-accent);
+};
+&:active {
+  background: var(--color-accent);
+  color: var(--color-background);
+}
+`
+export const ButtonStyle = styled('button')
+`
+    background: var(--color-background);
+    border: 0.0625rem solid var(--color-primary);
+    border-radius: 0.125rem;
+    color: var(--color-text);
+    font-size: ${props => props.small ? ".75rem" : "1rem"};
+    font-weight: 700;
+    height: ${props => props.small ? "1.75rem" : "2.5rem"};
+    margin: 0.5rem;
+    opacity: ${props => props.disabled && "0.5"};
+    line-height: 1rem;
+    transition: 0.1s ease;
+    width: ${props => props.small ? "3.625rem" : "7.5rem"};
+    cursor: ${props => props.disabled ? 'not-allowed;' :  '{}'};    
+    ${props => !props.disabled ? effs : ''};
+           
+`;
+
+const hover = 
+`{
+  background: var(--color-accent);
+  border-color: var(--color-accent);
+  color: var(--color-background);
+}`;
+export const highlightedButtonStyle = styled('div')
+`
+  background: var(--color-primaryLight);
+  border-color: var(--color-primaryLight);
+  color: var(--color-background);
+  &:hover: ${props => !props.disabled ? hover :  '{}'};
+  &:active {
+    border-color: var(--color-primary);
+  }
+`));
+
+
+const before =
+`
+{
+  content: "";
+  display: block;
+  height: 0;
+  width: 0;
+  margin-top: 0.75rem;
+} 
+`;
+
+export const ButtonInnerWrapper = styled('div')
+    `
+    display: flex;
+    align-items: center;
+    font-family: var(--font-family-secondary);
+    text-align: center;
+    justify-content: center;
+    /* Crop line height */
+    &:before: ${props => props.amount ? before :  '{}'};
+  `;
