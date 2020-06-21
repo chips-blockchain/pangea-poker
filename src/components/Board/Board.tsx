272b083a32ed661a242cbf54bbcebd3585c77f3a
piggydoughnut <drmikhailova@gmail.com>
2020-06-22 11:15:00 +1200

    Board styles

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,6 +1,7 @@
 import { css } from "@emotion/core";
 import Card, { CardFlip } from "../Card";
 import { GameTurns } from "../../lib/constants";
+import {BoardContainer, FlopElem, BoardCard1, BoardCard2, River } from "./css/BoardStyle";
 
 // This component renders the boardcards (the flop, turn and river)
 
@@ -13,63 +14,30 @@ const { preFlop } = GameTurns;
 
 const Board: React.FunctionComponent<IProps> = ({ boardCards, gameTurn }) => {
   return (
-    <div
-      css={css`
-        height: 4.25rem;
-        position: absolute;
-        top: 13.75rem;
-        left: 17rem;
-      `}
-    >
+    <BoardContainer>
       {/* Flop */}
-      <span
-        css={css`
-          opacity: ${gameTurn === preFlop ? "0" : "1"};
-          transition: 0s all;
-          transition-delay: 0.5s;
-        `}
-      >
+      <FlopElem gameTurn={gameTurn}>
         <span>
           <Card card={boardCards[0]} />
         </span>
-        <span
-          css={css`
-            position: relative;
-            right: ${gameTurn === preFlop ? "3.25rem" : "0"};
-            transition: 0.5s ease all;
-            transition-delay: 0.5s;
-          `}
-        >
+        <BoardCard1  gameTurn={gameTurn}>
           <Card card={boardCards[1]} />
-        </span>
-        <span
-          css={css`
-            position: relative;
-            right: ${gameTurn === preFlop ? "6.5rem" : "0"};
-            transition: 0.5s ease all;
-            transition-delay: 0.5s;
-          `}
-        >
+        </BoardCard1>
+        <BoardCard2  gameTurn={gameTurn}>
           <Card card={boardCards[2]} />
-        </span>
-      </span>
+        </BoardCard2>
+      </FlopElem>
       {/* Turn */}
-
       {boardCards.length >= 4 && (
         <CardFlip card={boardCards[3]} dealt={gameTurn >= 2} />
       )}
-      <span
-        css={css`
-          position: relative;
-          left: 0.125rem;
-        `}
-      >
+      <River>
         {/* River */}
         {boardCards.length === 5 && (
           <CardFlip card={boardCards[4]} dealt={gameTurn >= 3} />
         )}
-      </span>
-    </div>
+      </River>
+    </BoardContainer>
   );
 };
 
