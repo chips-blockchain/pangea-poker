c2b5233cf029651cb089c030bc34dda3522271c2
piggydoughnut <drmikhailova@gmail.com>
2020-06-22 18:11:22 +1200

    Player styles

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -19,6 +19,18 @@ import { IPlayer, IState } from "../../store/initialState";
 import { IMessage } from "../Game/onMessage";
 import { Possibilities, PlayerActions, GameTurns } from "../../lib/constants";
 import sounds from "../../sounds/sounds";
+import { 
+  Balance, 
+  CardsWrapper, 
+  faceDownCards, 
+  PlayerEmoji, 
+  playerWidget, 
+  PlayerName, 
+  PlayerInfo, 
+  PlayerTimerBar, 
+  PlayerHighlight,
+  PlayerNameWrapper
+ } from "./assets/style";
 
 // This is the Player widget that shows the player avatar, the chips amount, wether the player has cards, etc
 
@@ -78,81 +90,7 @@ const Player: React.FunctionComponent<IProps> = ({
   // State for counting the seconds
   const [secondsLeft, setSecondsLeft] = useState(timeAllowance);
 
-  // Styles
-
-  // Rules to change the colors when the time is low
-  const colorChange = (): string => {
-    return secondsLeft > timeAllowance * 0.25
-      ? "var(--color-accent)"
-      : "var(--color-danger)";
-  };
-
-  const Balance = styled.div`
-    color: var(--color-primaryLight);
-    font-size: var(--font-size-xs);
-    line-height: 1rem;
-    text-align: center;
-    text-transform: uppercase;
-  `;
-
-  const CardsWrapper = styled.div`
-    bottom: 0.875rem;
-    left: 1.75rem;
-    position: absolute;
-    opacity: ${winner && gameTurn === showDown && winner !== seat
-      ? "0.5"
-      : "1"};
-    z-index: 1;
-  `;
-
-  const faceDownCards = css`
-    bottom: 0;
-    left: 3rem;
-    position: absolute;
-    z-index: 1;
-  `;
-
-  const PlayerInfo = styled.div`
-   align-items: center;
-    display: grid;
-    background: var(--color-background);
-    border-radius: 10rem;
-    box-sizing: border-box;
-    box-shadow: inset 0 0 0.25rem rgba(255, 255, 255, 0.1);
-    /* ${isActive && "border: 2px solid " + colorChange() + ";"} */
-    border: 2px solid ${isActive ? colorChange() : "transparent"};
-    ${connected && "grid-template-columns: 1fr 0.5fr;"}
-    height: 100%;
-    justify-content: center;
-    transition: ${transitionSpeed};
-    position: absolute;
-    width: 100%;
-    z-index: 2;
-
-    &:hover div {
-      ${!connected && `color: var(--color-accent)`};
-    }
-  `;
-
-  const PlayerEmoji = styled.span`
-    font-size: var(--font-size-xl);
-    margin-right: 1rem;
-  `;
-
-  const PlayerName = styled.div`
-    color: ${lastAction.action && seat == playerIdToString(lastAction.player)
-      ? "var(--color-accent)"
-      : userName.color};
-    font-size: ${connected ? "0.625rem" : "1rem"};
-    line-height: 0.875rem;
-    text-align: center;
-    text-transform: uppercase;
-  `;
-
-  const playerWidget = css`
-    position: relative;
-    cursor: pointer;
-  `;
+  const playerNameColor = lastAction.action && seat == playerIdToString(lastAction.player) ? "var(--color-accent)" : userName.color;
 
   const handlePlayerClick = () => (): void => {
     if (!connected) {
@@ -260,13 +198,13 @@ const Player: React.FunctionComponent<IProps> = ({
           />
         </div>
       )}
-      <PlayerInfo>
-        <span
-          css={css`
-            ${connected && "margin-left: 1rem"};
-          `}
-        >
-          <PlayerName>
+      <PlayerInfo
+        isActive={isActive}
+        connected={connected}
+        secondsLeft={secondsLeft}
+      >
+        <PlayerNameWrapper connected={connected}>
+          <PlayerName color={playerNameColor} connected={connected} >
             {/* Show the player's name or the last action */}
             {!connected
               ? seatMessage
@@ -275,34 +213,20 @@ const Player: React.FunctionComponent<IProps> = ({
               : userName.text}
           </PlayerName>
           {connected && <Balance>{numberWithCommas(chips)}</Balance>}
-        </span>
+        </PlayerNameWrapper>
         {connected && <PlayerEmoji>{userAvatar}</PlayerEmoji>}
       </PlayerInfo>
       {/* Active player countdown */}
       {isActive && (
-        <div
-          css={css`
-            background: var(--color-background);
-            border: 2px solid ${colorChange()};
-            height: 0.5rem;
-            margin: auto;
-            position: relative;
-            top: 2.875rem;
-            transition: ${transitionSpeed}s;
-            width: 6.75rem;
-          `}
+        <PlayerHighlight
+          secondsLeft={secondsLeft}
           data-test="player-highlight"
         >
-          <div
-            css={css`
-              background-color: ${colorChange()};
-              height: 0.5rem;
-              width: ${(secondsLeft / timeAllowance) * 100}%;
-              transition: ${transitionSpeed}s;
-            `}
+          <PlayerTimerBar
+            secondsLeft={secondsLeft}
             data-test="player-timer-bar"
           />
-        </div>
+        </PlayerHighlight>
       )}
     </div>
   );
