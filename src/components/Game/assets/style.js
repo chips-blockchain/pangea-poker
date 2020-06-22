1e535dadbb0620e00d75bbdae621c9fcb1cc3d3e
piggydoughnut <drmikhailova@gmail.com>
2020-06-22 16:04:48 +1200

    game styles

---
+++ src/components/Game/assets/style.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const GameWrapper = styled('div')
+`
+    position: absolute;
+    z-index: 5;
+    top: 4;
+`
+
+export const DealerContainer = styled('div')
+`
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+`
